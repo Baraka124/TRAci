05aa46deebafb1ab0c75f8299e0b7f0d5aa083bc
Baraka124 <85062427+Baraka124@users.noreply.github.com>
2026-01-10 03:52:21 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,9 @@
-const CACHE = "projects-pwa-v1";
+const CACHE = "projects-pwa-v2";
 
 self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c =>
-      c.addAll(["./"])
-    )
-  );
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(["./"])));
 });
 
 self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
