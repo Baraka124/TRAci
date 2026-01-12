0248029e84a9a25ac15008ed5261bd0ada306064
Baraka124 <85062427+Baraka124@users.noreply.github.com>
2026-01-12 21:58:54 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE = "projects-pwa-v3";
-
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(["./"])));
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
